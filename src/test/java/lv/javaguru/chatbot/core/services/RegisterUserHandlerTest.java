dfb5806d4a2253d106ec7d1caa8a465a56bcd20c
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-10-26 19:06:23 +0200

    #11 TASK-5: Rewrite RegisterUserHandlerTest to real unit test

--- src/test/java/lv/javaguru/chatbot/core/services/RegisterUserHandlerTest.java
+++ src/test/java/lv/javaguru/chatbot/core/services/RegisterUserHandlerTest.java
@@ -1,49 +1,37 @@
 package lv.javaguru.chatbot.core.services;
 
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
+
 import lv.javaguru.chatbot.core.DomainCommandResult;
 import lv.javaguru.chatbot.core.commands.RegisterUserCommand;
 import lv.javaguru.chatbot.core.domain.User;
 import lv.javaguru.chatbot.core.persistence.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.util.ReflectionTestUtils;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+
+import java.util.Optional;
 
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
-@SpringBootTest
-@RunWith(SpringRunner.class)
-@TestExecutionListeners({
-        DependencyInjectionTestExecutionListener.class,
-        DbUnitTestExecutionListener.class
-})
+@RunWith(MockitoJUnitRunner.class)
 public class RegisterUserHandlerTest {
-    private final User user = new User();
-    @Autowired
+
+    @Mock
     UserRepository userRepository;
 
-    @Test
-    public void shouldReturnErrorWhenTelegramIdNullOrEmpty_1() {
-        RegisterUserHandler registerUserHandler = new RegisterUserHandler();
-        RegisterUserCommand command = new RegisterUserCommand("");
-        DomainCommandResult result = registerUserHandler.execute(command);
-        assertTrue(result.hasErrors());
-        assertEquals(result.getErrors().size(), 1);
-        assertEquals(result.getErrors().get(0).getField(), "telegramId");
-        assertEquals(result.getErrors().get(0).getMessage(), "Must be not empty!");
-    }
+    @InjectMocks
+    private RegisterUserHandler handler;
+
 
     @Test
-    public void shouldReturnErrorWhenTelegramIdNullOrEmpty_2() {
-        RegisterUserHandler registerUserHandler = new RegisterUserHandler();
+    public void shouldReturnErrorWhenTelegramIdNull() {
         RegisterUserCommand command = new RegisterUserCommand(null);
-        DomainCommandResult result = registerUserHandler.execute(command);
+        DomainCommandResult result = handler.execute(command);
         assertTrue(result.hasErrors());
         assertEquals(result.getErrors().size(), 1);
         assertEquals(result.getErrors().get(0).getField(), "telegramId");
@@ -51,33 +39,34 @@ public class RegisterUserHandlerTest {
     }
 
     @Test
-    public void shouldReturnNoErrorUserNotExists() {
-        RegisterUserHandler registerUserHandler = new RegisterUserHandler();
-        RegisterUserCommand command = new RegisterUserCommand("111");
+    public void shouldReturnErrorWhenTelegramIdEmpty() {
+        RegisterUserCommand command = new RegisterUserCommand("");
+        DomainCommandResult result = handler.execute(command);
+        assertTrue(result.hasErrors());
+        assertEquals(result.getErrors().size(), 1);
+        assertEquals(result.getErrors().get(0).getField(), "telegramId");
+        assertEquals(result.getErrors().get(0).getMessage(), "Must be not empty!");
+    }
 
-        ReflectionTestUtils.setField(registerUserHandler, "userRepository", userRepository);
-
-        DomainCommandResult result = registerUserHandler.execute(command);
+    @Test
+    public void shouldCreateNewUser() {
+        Mockito.when(userRepository.findByTelegramId("25")).thenReturn(Optional.empty());
+        RegisterUserCommand command = new RegisterUserCommand("25");
+        DomainCommandResult result = handler.execute(command);
         assertFalse(result.hasErrors());
     }
 
     @Test
     public void shouldReturnErrorUserWithTelegramIdAlreadyExist() {
-        RegisterUserHandler registerUserHandler = new RegisterUserHandler();
-        RegisterUserCommand command = new RegisterUserCommand("111");
-
-        user.setTelegramId("111");
-        userRepository.save(user);
-
-        ReflectionTestUtils.setField(registerUserHandler, "userRepository", userRepository);
-
-        DomainCommandResult result = registerUserHandler.execute(command);
+        User user = new User();
+        user.setTelegramId("25");
+        Mockito.when(userRepository.findByTelegramId("25")).thenReturn(Optional.of(user));
+        RegisterUserCommand command = new RegisterUserCommand("25");
+        DomainCommandResult result = handler.execute(command);
         assertTrue(result.hasErrors());
         assertEquals(result.getErrors().size(), 1);
         assertEquals(result.getErrors().get(0).getField(), "telegramId");
-        assertEquals(result.getErrors().get(0).getMessage(), "User with telegram id " + "111" + " already exist!");
-        userRepository.delete(user);
+        assertEquals(result.getErrors().get(0).getMessage(), "User with telegram id 25 already exist!");
     }
-
 
 }
