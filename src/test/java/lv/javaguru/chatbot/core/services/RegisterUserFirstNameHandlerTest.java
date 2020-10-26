126acaabccb5e69767bb88fca08470052153d3b2
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-10-26 12:35:11 +0200

    #10 TASK-4: Rewrite RegisterUserFirstNameHandlerTest to real unit test

--- src/test/java/lv/javaguru/chatbot/core/services/RegisterUserFirstNameHandlerTest.java
+++ src/test/java/lv/javaguru/chatbot/core/services/RegisterUserFirstNameHandlerTest.java
@@ -1,37 +1,31 @@
 package lv.javaguru.chatbot.core.services;
 
-
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import lv.javaguru.chatbot.core.DomainCommandResult;
 import lv.javaguru.chatbot.core.commands.RegisterUserFirstNameCommand;
 import lv.javaguru.chatbot.core.domain.User;
 import lv.javaguru.chatbot.core.persistence.UserRepository;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.junit.Test;
-import org.springframework.test.util.ReflectionTestUtils;
-
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 import static org.junit.Assert.*;
 
-@SpringBootTest
-@RunWith(SpringRunner.class)
-@TestExecutionListeners({
-        DependencyInjectionTestExecutionListener.class,
-        DbUnitTestExecutionListener.class
-})
+import java.util.Optional;
+
+@RunWith(MockitoJUnitRunner.class)
 public class RegisterUserFirstNameHandlerTest {
-    private final User user = new User();
-    @Autowired UserRepository userRepository;
+
+    @Mock UserRepository userRepository;
+
+    @InjectMocks
+    private RegisterUserFirstNameHandler handler;
 
     @Test
-    public void shouldReturnErrorWhenTelegramIdNullOrEmpty_1() {
-        RegisterUserFirstNameHandler registerUserFirstNameHandler = new RegisterUserFirstNameHandler();
-        RegisterUserFirstNameCommand command = new RegisterUserFirstNameCommand("", "User Name");
-        DomainCommandResult result = registerUserFirstNameHandler.execute(command);
+    public void shouldReturnErrorWhenTelegramIdNull() {
+        RegisterUserFirstNameCommand command = new RegisterUserFirstNameCommand(null, "FirstName");
+        DomainCommandResult result = handler.execute(command);
         assertTrue(result.hasErrors());
         assertEquals(result.getErrors().size(), 1);
         assertEquals(result.getErrors().get(0).getField(), "telegramId");
@@ -39,10 +33,9 @@ public class RegisterUserFirstNameHandlerTest {
     }
 
     @Test
-    public void shouldReturnErrorWhenTelegramIdNullOrEmpty_2() {
-        RegisterUserFirstNameHandler registerUserFirstNameHandler = new RegisterUserFirstNameHandler();
-        RegisterUserFirstNameCommand command = new RegisterUserFirstNameCommand(null, "User Name");
-        DomainCommandResult result = registerUserFirstNameHandler.execute(command);
+    public void shouldReturnErrorWhenTelegramIdEmpty() {
+        RegisterUserFirstNameCommand command = new RegisterUserFirstNameCommand("", "FirstName");
+        DomainCommandResult result = handler.execute(command);
         assertTrue(result.hasErrors());
         assertEquals(result.getErrors().size(), 1);
         assertEquals(result.getErrors().get(0).getField(), "telegramId");
@@ -51,34 +44,25 @@ public class RegisterUserFirstNameHandlerTest {
 
     @Test
     public void shouldUpdateUserFirstName() {
-        RegisterUserFirstNameHandler registerUserFirstNameHandler = new RegisterUserFirstNameHandler();
-        RegisterUserFirstNameCommand command = new RegisterUserFirstNameCommand("111", "User Name");
-
-        user.setTelegramId("111");
-        userRepository.save(user);
-
-        ReflectionTestUtils.setField(registerUserFirstNameHandler, "userRepository", userRepository);
-
-        DomainCommandResult result = registerUserFirstNameHandler.execute(command);
+        User user = new User();
+        user.setTelegramId("11");
+        user.setFirstName(null);
+        Mockito.when(userRepository.findByTelegramId("11")).thenReturn(Optional.of(user));
+        RegisterUserFirstNameCommand command = new RegisterUserFirstNameCommand("11", "FirstName");
+        DomainCommandResult result = handler.execute(command);
         assertFalse(result.hasErrors());
-        userRepository.delete(user);
+        assertEquals(user.getFirstName(), "FirstName");
     }
 
     @Test
     public void shouldReturnErrorNoUserWithTelegramId() {
-        RegisterUserFirstNameHandler registerUserFirstNameHandler = new RegisterUserFirstNameHandler();
-        RegisterUserFirstNameCommand command = new RegisterUserFirstNameCommand("222", "User Name");
-
-        user.setTelegramId("111");
-        userRepository.save(user);
-
-        ReflectionTestUtils.setField(registerUserFirstNameHandler, "userRepository", userRepository);
-
-        DomainCommandResult result = registerUserFirstNameHandler.execute(command);
+        Mockito.when(userRepository.findByTelegramId("25")).thenReturn(Optional.empty());
+        RegisterUserFirstNameCommand command = new RegisterUserFirstNameCommand("25", "FirstName");
+        DomainCommandResult result = handler.execute(command);
         assertTrue(result.hasErrors());
         assertEquals(result.getErrors().size(), 1);
         assertEquals(result.getErrors().get(0).getField(), "telegramId");
-        assertEquals(result.getErrors().get(0).getMessage(), "User with telegram id " + "222" + " not found!");
-        userRepository.delete(user);
+        assertEquals(result.getErrors().get(0).getMessage(), "User with telegram id " + "25" + " not found!");
     }
+
 }
